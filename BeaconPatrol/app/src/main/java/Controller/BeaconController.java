3b015307182c5d6062c4737daf8d2c4495b2d33a
HuaSwee <hallofmirage@gmail.com>
2017-11-22 19:01:56 +0800

    added lock and unlock bike methods in BeaconController
    call BeaconController.lockBike(String beaconID) AND
    call BeaconController.unlockBike(String beaconID)
    
    to lock and unlock the bike and send the data to the web server

--- BeaconPatrol/app/src/main/java/Controller/BeaconController.java
+++ BeaconPatrol/app/src/main/java/Controller/BeaconController.java
@@ -25,6 +25,8 @@ public class BeaconController {
 
     static String beaconServer = "http://139.59.228.105:80/bikes/bp/";
     static String beaconCAP = "getCurrentCap/?bID=";
+    static String lockBike = "parkBike/?bID=";
+    static String unlockBike = "unlockBike/?bID=";
     static BufferedReader rd;
 
 
@@ -54,14 +56,77 @@ public class BeaconController {
                 Log.e("GETBEACONCAP", output);
 				JSONObject jsonObj = new JSONObject(output);
                 result = jsonObj.getInt("CurrentCapacity");
-                Log.w("GETBEACONCAP", "Result = " + result);            }
-
+                Log.w("GETBEACONCAP", "Result = " + result);
+            }
+            conn.disconnect();
         } catch (Exception e) {
             Log.d("GETBEACONCAP", e.getStackTrace().toString());
         }
 
         Log.d("GETBEACONCAP", "result = " + result);
+
         return result;
+
+    }
+
+    public static void lockBike(String beaconID) {
+        try {
+            URL url = new URL(beaconServer  + lockBike + beaconID);
+            Log.d("LOCKBIKE", "url = " + url);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(false);
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("User-Agent", "Mozilla/5.0 ( compatible ) ");
+            conn.setRequestProperty("Accept", "*/*");
+            //wr = new OutputStreamWriter(conn.getOutputStream());
+            conn.connect();
+            int status = conn.getResponseCode();
+            Log.d("LOCKBIKE", Integer.toString(status));
+            Log.d("LOCKBIKE", "Bike Sent");
+            // Get the response
+//            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+//            String output = null;
+//            while ((output = rd.readLine()) != null) {
+//                Log.e("GETBEACONCAP", output);
+//                JSONObject jsonObj = new JSONObject(output);
+//                result = jsonObj.getInt("CurrentCapacity");
+//                Log.w("GETBEACONCAP", "Result = " + result);            }
+            conn.disconnect();
+        } catch (Exception e) {
+            Log.d("LOCKBIKE", e.getStackTrace().toString());
+        }
+
+
+
+    }
+
+    public static void unlockBike(String beaconID) {
+
+        try {
+            URL url = new URL(beaconServer  + unlockBike + beaconID);
+            Log.d("UNLOCKBIKE", "url = " + url);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(false);
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("User-Agent", "Mozilla/5.0 ( compatible ) ");
+            conn.setRequestProperty("Accept", "*/*");
+            //wr = new OutputStreamWriter(conn.getOutputStream());
+            conn.connect();
+            int status = conn.getResponseCode();
+            Log.d("UNLOCKBIKE", Integer.toString(status));
+            Log.d("UNLOCKBIKE", "Bike Sent");
+            // Get the response
+//            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+//            String output = null;
+//            while ((output = rd.readLine()) != null) {
+//                Log.e("GETBEACONCAP", output);
+//                JSONObject jsonObj = new JSONObject(output);
+//                result = jsonObj.getInt("CurrentCapacity");
+//                Log.w("GETBEACONCAP", "Result = " + result);            }
+            conn.disconnect();
+        } catch (Exception e) {
+            Log.d("UNLOCKBIKE", e.getStackTrace().toString());
+        }
 
     }
 
